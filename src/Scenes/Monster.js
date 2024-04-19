f0e07ea8cb9aa2372e18b7af0559d5b29e6aa64e
ElaGolubkova <egolubko@ucsc.edu>
2024-04-19 15:29:48 -0700

    added a monster with smile or fangs

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -6,7 +6,32 @@ class Monster extends Phaser.Scene {
         //Create constants for the monster location
         this.bodyX = 300;
         this.bodyY = 350;
-        
+
+        //Smile
+        this.smileX = this.bodyX;
+        this.smileY = this.bodyY + 30;
+
+        //Arm
+        this.leftArmX = this.bodyX - 105;
+        this.leftArmY = this.bodyY + 85;
+
+        this.rightArmX = this.bodyX + 105;
+        this.rightArmY = this.bodyY + 85;
+
+        //Legs
+        this.leftLegX = this.bodyX - 40;
+        this.leftLegY = this.bodyY + 125;
+
+        this.rightLegX = this.bodyX + 40;
+        this.rightLegY = this.bodyY + 125;
+
+        //Eyes
+        this.leftEyeX = this.bodyX - 40;
+        this.leftEyeY = this.bodyY - 30;
+
+        this.rightEyeX = this.bodyX + 40;
+        this.rightEyeY = this.bodyY - 30;
+
     }
 
     // Use preload to load art and sound assets before the scene starts running.
@@ -31,9 +56,52 @@ class Monster extends Phaser.Scene {
         //
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
+        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_blueF.png");
 
-        
+        // Create the two sprites, one for each type of smile
+        my.sprite.smile = this.add.sprite(this.smileX, this.smileY, "monsterParts", "mouthA.png");
+        my.sprite.fangs = this.add.sprite(this.smileX, this.smileY, "monsterParts", "mouthB.png");
+        my.sprite.fangs.visible = false;
+
+        //Create sprites for Arms
+        my.sprite.rightArm = this.add.sprite(this.rightArmX, this.rightArmY, "monsterParts", "arm_blueA.png");
+        my.sprite.leftArm = this.add.sprite(this.leftArmX, this.leftArmY, "monsterParts", "arm_blueA.png");
+        my.sprite.leftArm.flipX = true;
+
+        //Create sprites for Legs
+        my.sprite.rightLeg = this.add.sprite(this.rightLegX, this.rightLegY, "monsterParts", "leg_blueC.png");
+        my.sprite.leftLeg = this.add.sprite(this.leftLegX, this.leftLegY, "monsterParts", "leg_blueC.png");
+        my.sprite.leftLeg.flipX = true;
+
+        //Create sprites for Eyes
+        my.sprite.rightEye = this.add.sprite(this.rightEyeX, this.rightEyeY, "monsterParts", "eye_cute_light.png");
+        my.sprite.leftEye = this.add.sprite(this.leftEyeX, this.leftEyeY, "monsterParts", "eye_cute_light.png");
+        my.sprite.leftEye.flipX = true;
+
+        //Scale Eyes (if needed)
+        // my.sprite.leftEye.setScale(0.7);
+        // my.sprite.rightEye.setScale(0.7);
+
+
+        // "Event input:smile"
+        let sKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+
+        sKey.on('down', (key, event) => {
+            my.sprite.fangs.visible = false;
+            my.sprite.smile.visible = true;
+	    });
+
+        // "Event input: fangs"
+        let fKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+
+        fKey.on('down', (key, event) => {
+            my.sprite.fangs.visible = true;
+            my.sprite.smile.visible = false;
+	    });
+
+
+
+
     }
 
     update() {
